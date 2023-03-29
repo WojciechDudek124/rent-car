8be26a9d7ed3ef86b384dd3c900788f012488144
Wojtek Dudek <wojciech.dudek124@gmail.com>
2023-03-29 22:57:17 +0200

    Start create car section from beggining. Add to section new slider and add some style, but it's not ready yet

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
